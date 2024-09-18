58cca46a8bfc0d15187f7db4f10b51fedd760810
Muspi Merol <wjj123345567@icloud.com>
2024-09-18 18:54:23 +0800

    update prompt and set token threshold

--- app/core/api.py
+++ app/core/api.py
@@ -1,19 +1,24 @@
 from json import loads
 
+from promptools.openai import count_token
+
 from ..utils.dom import compress_spaces
 from ..utils.llm import complete
 from .parse import get_cleaned_dom
 from .prompt import main_loop
 
+MAX_TOKENS = 300_000
+
 
 async def auto_annotate(url: str) -> dict[str, str]:
     dom = await get_cleaned_dom(url)
+    html = compress_spaces(dom.css("body").get())  # type: ignore
+
+    if count_token(html) > MAX_TOKENS:
+        return {}
 
     context = await main_loop.ainvoke(
-        {
-            "dom": dom,
-            "html": compress_spaces(dom.css("body").get()),  # type: ignore
-        },
+        {"dom": dom, "html": html},
         complete=complete,
         temperature=0,
     )
